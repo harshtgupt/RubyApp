f9df16aab8d9e076b635321bf3b04d288d0457f9
unknown <gupha01@GUPHA01-W7.ca.com>
2015-04-22 02:02:22 +0530

    too much changes

--- main.rb
+++ main.rb
@@ -1,32 +1,7 @@
 # encoding: UTF-8
-require 'json'
 require 'sinatra'
-require 'data_mapper'
-require 'dm-migrations'
+
 
 get '/' do
   "Hello World!"
 end
-
-configure :development do
-  enable :cross_origin
-  DataMapper::Logger.new($stdout, :debug)
-  DataMapper.setup(
-    :default,
-    ENV['DATABASE_URL'] || 'postgres://localhost/mydb'
-  )
-end
-
-configure :production do
-  enable :cross_origin
-  DataMapper.setup(
-    :default,
-    ENV['DATABASE_URL'] || 'postgres://localhost/mydb'
-  )
-end
-
-require './models/init'
-require './helpers/init'
-require './routes/init'
-
-DataMapper.finalize
