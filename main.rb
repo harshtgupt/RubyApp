062396263ca3225419cb771136a5e60246538c01
unknown <gupha01@GUPHA01-W7.ca.com>
2015-04-22 02:16:39 +0530

    more changes

--- main.rb
+++ main.rb
@@ -1,8 +1,33 @@
 # encoding: UTF-8
 require 'sinatra'
 require 'rubygems'
-
+require 'json'
+require 'data_mapper'
+require 'dm-migrations'
 
 get '/' do
   "Hello World!"
 end
+
+configure :development do
+  enable :cross_origin
+  DataMapper::Logger.new($stdout, :debug)
+  DataMapper.setup(
+    :default,
+    ENV['DATABASE_URL'] || 'postgres://localhost/mydb'
+  )
+end
+
+configure :production do
+  enable :cross_origin
+  DataMapper.setup(
+    :default,
+    ENV['DATABASE_URL'] || 'postgres://localhost/mydb'
+  )
+end
+
+require './models/init'
+require './helpers/init'
+require './routes/init'
+
+DataMapper.finalize
